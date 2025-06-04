9a4cd59bdb6649e37e8aa193111fe106ab4fa529
Lucas Barão <lucas.baraom@gmail.com>
2025-06-04 02:25:14 -0300

    configuração incial do projeto

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\DicomImageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('dicom-images', DicomImageController::class);
