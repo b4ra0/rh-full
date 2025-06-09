2a2e2389491d6b0c38bfbaa29b78a4e633d98428
Lucas Barão <lucas.baraom@gmail.com>
2025-06-08 23:03:28 -0300

    criando o http client e removendo arquivos que não serão usados

---
+++ frontend/src/api/httpClient.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const httpClient = axios.create({
+    baseURL: 'http://localhost:80/',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
